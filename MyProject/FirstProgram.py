e816ba53bfe15edea0d884d02e6037e0372adba2
Python3Dev <zzcall106@gmail.com>
2025-01-11 07:20:49 +0800

    Creat Index.py and MyProject.py

---
+++ MyProject/FirstProgram.py
@@ -0,0 +1,16 @@
+H = "Hello, World!\n"
+print(H)
+
+###################################################
+#  This Command to do a comment Unvisibel         #
+#   print('Hello, World\n\n')                     #
+#   print('''Hello, World\n\n''')                 #
+#   print("Hello, World\n\n")                     #
+#   print("""Hello, World\n\n""")                 #
+###################################################
+
+print("clear")
+
+
+Z = "\n\n Author: \"Python3Dev\""
+print(Z)
